cfd0d9bc4ff98cbb45dd5f7ca0e85254b6e9bab0
Adam Svoboda <eidamsvoboda@gmail.com>
2023-05-01 20:47:58 +0200

    disable mobile touch event type

--- js/game_loop.js
+++ js/game_loop.js
@@ -1068,11 +1068,12 @@ function registerClickListeners(){
   //Essential, onTouchEnd event type usage is needed on mobile to enable vibration effects
   //Breaks interactions on loading the page using Dev Tools "mobile preview" followed by switching it off
   var eventType = 'click';
-  if (String(navigator.userAgentData) != "undefined"){ //Any browser except Chrome needs this, it took only 3 hours to realize
-    if (navigator.userAgentData.mobile){
-      eventType = 'touchend';
-    }
-  }
+  //if (String(navigator.userAgentData) != "undefined"){ //Any browser except Chrome needs this, it took only 3 hours to realize
+  //  if (navigator.userAgentData.mobile){
+  //    eventType = 'touchend';
+  //  }
+  //}
+  console.log("platform interaction event type="+eventType);
   document.getElementById('button_attack').addEventListener(eventType, resolveAction('button_attack'));
   document.getElementById('button_block').addEventListener(eventType, resolveAction('button_block'));
   document.getElementById('button_roll').addEventListener(eventType, resolveAction('button_roll'));
