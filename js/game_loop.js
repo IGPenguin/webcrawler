aede794fc01d98b2ceac6d30a3afaca8186687d5
Adam Svoboda <eidamsvoboda@gmail.com>
2023-05-02 21:22:17 +0200

    Remove extra word from log

--- js/game_loop.js
+++ js/game_loop.js
@@ -309,7 +309,7 @@ function resolveAction(button){ //Yeah, this is bad, like really bad
             nextEncounter();
             break;
           default:
-            logPlayerAction(actionString,"Your attack attempt had no effect -1 🟢");
+            logPlayerAction(actionString,"Your attack had no effect -1 🟢");
             displayEnemyEffect("〽️");
       }
       break;
