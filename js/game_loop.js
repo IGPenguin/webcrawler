7e41f5030d2b90fca029a61f898eea885915b303
Adam Svoboda <eidamsvoboda@gmail.com>
2024-10-25 12:44:17 +0200

    Disable tutorial skip, bump version

--- js/game_loop.js
+++ js/game_loop.js
@@ -2,8 +2,8 @@
 //...submit a pull request if you dare
 
 //Debug
-var versionCode = "fpm 10/25/24 • 1:22 pm"
-var initialEncounterOverride=7; //7 skips tutorial
+var versionCode = "fpm 10/25/24 • 12:43 pm"
+var initialEncounterOverride=0; //7 skips tutorial
 if (initialEncounterOverride!=0) initialEncounterOverride-=3; //To handle notes and death in .csv
 
 //Colors
