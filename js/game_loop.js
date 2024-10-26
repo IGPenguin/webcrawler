e43067de21b3e5b6277524103235819d552fd975
Adam Svoboda <eidamsvoboda@gmail.com>
2024-10-27 01:14:58 +0200

    Hotfixes + shark surprise

--- js/game_loop.js
+++ js/game_loop.js
@@ -2,8 +2,8 @@
 //...submit a pull request if you dare
 
 //Debug
-var versionCode = "fpm 10/27/24 • 0:58 am"
-var initialEncounterOverride=7; //7 skips tutorial
+var versionCode = "fpm 10/27/24 • 1:12 am"
+var initialEncounterOverride=0; //7 skips tutorial
 if (initialEncounterOverride!=0) initialEncounterOverride-=3; //To handle notes and death in .csv
 
 //Colors
