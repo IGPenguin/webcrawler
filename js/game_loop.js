8b7d254aabd314cb05f801777d7f1450a1ba1535
Adam Svoboda <eidamsvoboda@gmail.com>
2024-10-28 04:27:39 +0100

    Revert debug-only changes

--- js/game_loop.js
+++ js/game_loop.js
@@ -52,9 +52,9 @@ var seenLoot;
 renewPlayer();
 function renewPlayer(){ //Default values
   playerName = getFirstName();
-  playerHpMax=2;
+  playerHpMax=3;
   playerHp = playerHpMax;
-  playerStaMax = 1;
+  playerStaMax = 3;
   playerSta = 0; //Start tired in a dream (was playerStaMax;)
   playerMgkMax = 0;
   playerAtk = 1;
