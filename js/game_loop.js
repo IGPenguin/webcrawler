adfb81db09ea49ce4482f8110cb2f3556e374815
Adam Svoboda <eidamsvoboda@gmail.com>
2024-11-13 23:26:43 +0100

    bump version

--- js/game_loop.js
+++ js/game_loop.js
@@ -2,7 +2,7 @@
 //...submit a pull request if you dare
 
 //Debug
-var versionCode = "ver. 11/13/24 • 4:46 pm"
+var versionCode = "ver. 11/13/24 • 11:26 pm"
 var initialEncounterOverride=0; //6 skips tutorial
 
 //To handle notes and death in .csv
