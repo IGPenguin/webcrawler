969ff8a8ac071e2d677bd7fe4ecb984e578c53c5
Adam Svoboda <eidamsvoboda@gmail.com>
2025-04-18 19:31:22 +0200

    cleanup some mess, bump version

--- js/game_loop.js
+++ js/game_loop.js
@@ -2,7 +2,7 @@
 //...submit a pull request if you dare
 
 //Debug
-var versionCode = "ver. 04/18/25 • 12:35pm"
+var versionCode = "ver. 04/18/25 • 07:27pm"
 var initialEncounterOverride=0; //6 skips tutorial, ~38 barrens
 if (initialEncounterOverride!=0) initialEncounterOverride-=3; //To handle notes and death in .csv
 
