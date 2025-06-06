b70eb2b79632c621a1c19672356b517955c4beb9
Adam Svoboda <eidamsvoboda@gmail.com>
2025-06-07 00:50:31 +0200

    tweak lvl and chick

--- js/game_loop.js
+++ js/game_loop.js
@@ -60,7 +60,7 @@ function renewPlayer(){ //Default values
   playerInt = 1;
   playerXP=0;
   playerLevel=1;
-  playerXPThreshold=150;
+  playerXPThreshold=200;
   playerMgk = playerMgkMax;
   playerRested = false;
   playerLootString = "";
