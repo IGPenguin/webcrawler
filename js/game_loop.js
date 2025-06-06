bae2e03dee1b0ee0787c95916c9330ed5090740e
Adam Svoboda <eidamsvoboda@gmail.com>
2025-06-06 18:41:57 +0200

    rename necropolis, tweaks to whatever stuff, drop in generated data, blah blah

--- js/game_loop.js
+++ js/game_loop.js
@@ -2600,7 +2600,7 @@ function enemyAttackOrRest(message="",isGrab=false){
     if (damageReceived<=0){
       staminaChangeMsg=chooseFrom(["They just hang around.","They do not seem to care.","They just wait around.","They seem to be very chill."])
       if (enemyCursed && (enemyAtk+enemyAtkBonus)<=0) staminaChangeMsg="They are too weak to do any harm."
-      animateUIElement(emojiWrapperUIElement,"animate__headShake","1"); //Play chill animation
+      animateUIElement(emojiWrapperUIElement,"animate__headShake","0.8"); //Play chill animation
       if (enemyType=="Pet"){ //Harder to befriend
         enemyIntBonus++;
         if ((enemyInt+enemyIntBonus)<=playerInt){
