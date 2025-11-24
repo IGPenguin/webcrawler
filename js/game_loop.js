be699f85c3bb37d52acbb42600bc0ac5951300c7
Adam Svoboda <eidamsvoboda@gmail.com>
2025-11-24 23:20:54 +0100

    big ol' version bump

--- js/game_loop.js
+++ js/game_loop.js
@@ -2,7 +2,7 @@
 //...submit a pull request if you dare
 
 //Debug
-var versionCode = "ver. 11/14/2025 @ 07:03 PM"
+var versionCode = "ver. 11/24/2025 @ 11:20 PM"
 var initialEncounterOverride=0; //6 skips tutorial
 if (location.hostname === "localhost" || location.hostname === "127.0.0.1") initialEncounterOverride=4;
 
