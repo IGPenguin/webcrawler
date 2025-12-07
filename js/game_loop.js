1cb7d9103eb5cbcd611cc915a913590160a73b5b
igpenguin <eidamsvoboda@gmail.com>
2025-12-08 00:40:25 +0100

    bump version

--- js/game_loop.js
+++ js/game_loop.js
@@ -2,7 +2,7 @@
 //...submit a pull request if you dare
 
 //Debug
-var versionCode = "ver. 12/08/2025 @ 00:16 PM"
+var versionCode = "ver. 12/08/2025 @ 00:40 PM"
 var initialEncounterOverride=0; //6 skips tutorial
 if (location.hostname === "localhost" || location.hostname === "127.0.0.1") initialEncounterOverride=4;
 
