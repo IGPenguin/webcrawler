e585fa67884b2714a43d2eedc06af39c28f12571
igpenguin <eidamsvoboda@gmail.com>
2025-12-31 03:25:50 +0100

    bump version, not done yet, but getting real close

--- js/game_loop.js
+++ js/game_loop.js
@@ -2,7 +2,7 @@
 //Note: This game was not planned for 2+ years of development 💀
 
 //Debug
-var versionCode = "ver. 12/31/2025 @ 01:09 AM"
+var versionCode = "ver. 12/31/2025 @ 03:25 AM"
 var initialEncounterOverride=0; //6 skips tutorial
 
 function isLocalhost(){ if (location.hostname === "localhost" || location.hostname === "127.0.0.1" || location.hostname.includes("192.168")) return true;}
