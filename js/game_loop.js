b816c604288e903643ba3b4e905a1651bd8c28fd
igpenguin <eidamsvoboda@gmail.com>
2026-04-01 21:40:03 +0200

    bump version, text tweak

--- js/game_loop.js
+++ js/game_loop.js
@@ -2,7 +2,7 @@
 //Note: This game was not planned for 2+ years of development 💀
 
 //Debug
-var versionCode = "ver. 02/27/2026 @ 11:55 PM"
+var versionCode = "ver. 04/01/2026 @ 09:39 PM"
 var initialEncounterOverride=0; //6 skips tutorial
 
 function isLocalhost(){ if (location.hostname === "localhost" || location.hostname === "127.0.0.1" || location.hostname.includes("192.168")) return true;}
