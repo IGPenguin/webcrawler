120371852ba6f62006fa0bd707d224cd26592a9f
Adam Svoboda <eidamsvoboda@gmail.com>
2026-04-10 22:24:33 +0200

    big old version bump

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 //Note: This game was not planned for 2+ years of development 💀
 
 //Debug
-var versionCode = "ver. 04/10/2026 @ 09:02 AM"
+var versionCode = "ver. 04/10/2026 @ 10:24 PM"
 var initialEncounterOverride=0; //6 skips tutorial
 
 function isLocalhost(){ if (location.hostname === "localhost" || location.hostname === "127.0.0.1" || location.hostname.includes("192.168")) return true;}
--- version.sh
+++ version.sh
@@ -4,6 +4,6 @@
 TIMESTAMP=$(date "+%m/%d/%Y @ %I:%M %p")
 NEW_VERSION="ver. $TIMESTAMP"
 
-sed -i '' "s/var versionCode = \"ver\. [^\"]*\"/var versionCode = \"$NEW_VERSION\"/" js/config.js
+sed -i '' "s|var versionCode = \"ver\. [^\"]*\"|var versionCode = \"$NEW_VERSION\"|" js/config.js
 
 echo "Version updated to: $NEW_VERSION"
