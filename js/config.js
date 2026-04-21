77a4c9878c1ece2d62a33fcdd9515340bc55e935
Adam Svoboda <eidamsvoboda@gmail.com>
2026-04-22 00:33:39 +0200

    bump version

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 //Debug
-var versionCode = "ver. 04/21/2026 @ 11:47 PM"
+var versionCode = "ver. 04/22/2026 @ 12:33 AM"
 var initialEncounterOverride=0; //6 skips tutorial
 
 function isLocalhost(){ if (location.hostname === "localhost" || location.hostname === "127.0.0.1" || location.hostname.includes("192.168")) return true;}
