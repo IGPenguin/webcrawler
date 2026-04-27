3a5c93c10eaf17335d6da551e0a808e32e657734
Adam Svoboda <eidamsvoboda@gmail.com>
2026-04-27 22:52:00 +0200

    bump version

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 // ── Debug / Version ───────────────────────────────────────────────────────────
-var versionCode = "ver. 04/27/2026 @ 01:26 AM"
+var versionCode = "ver. 04/27/2026 @ 10:51 PM"
 var initialEncounterOverride = 0; // set to 7 to skip tutorial (must equal Depths of Slumber row count + 2)
 
 function isLocalhost() {
