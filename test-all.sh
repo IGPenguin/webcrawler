33e0468d62401bf3ded1e30b07f29cfb18ceb5bf
Adam Svoboda <eidamsvoboda@gmail.com>
2026-04-29 22:20:52 +0200

    split run all validations/tests .sh files

--- test-all.sh
+++ test-all.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 set -e
-bash validate-js.sh
-bash validate-csv.sh
-bash validate-html.sh
+
 NODE_PATH="$(npm root -g)" playwright test automated-tests/boot.spec.js automated-tests/encounters.spec.js automated-tests/rarity.spec.js --config automated-tests/playwright.config.js "$@"
