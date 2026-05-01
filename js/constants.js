905cac73e89858809fba305bc7f95d232605045f
Adam Svoboda <eidamsvoboda@gmail.com>
2026-05-02 01:59:50 +0200

    bump version

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 // ── Debug / Version ───────────────────────────────────────────────────────────
-var versionCode = "ver. 05/02/2026 @ 01:54 AM"
+var versionCode = "ver. 05/02/2026 @ 01:59 AM"
 var initialEncounterOverride = 0; // set to 7 to skip tutorial (must equal Depths of Slumber row count + 2)
 var TUTORIAL_SKIP_LOCALHOST = false;
 var RANKINGS_DISABLED_LOCALHOST = false;
