46b4430872d9f04eaffef881a474a7568d7e4f26
Adam Svoboda <eidamsvoboda@gmail.com>
2026-05-10 00:41:06 +0200

    bump version

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 // ── Debug / Version ───────────────────────────────────────────────────────────
-var versionCode = "ver. 05/10/2026 @ 12:19 AM"
+var versionCode = "ver. 05/10/2026 @ 12:41 AM"
 var initialEncounterOverride = 0; // set to 5 to skip tutorial
 var TUTORIAL_SKIP_LOCALHOST = true;
 var RANKINGS_DISABLED_LOCALHOST = false;
