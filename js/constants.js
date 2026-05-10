840c864f49cbf7349c31d61581a536c1cf97908e
Adam Svoboda <eidamsvoboda@gmail.com>
2026-05-11 00:43:28 +0200

    bump version

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 // ── Debug / Version ───────────────────────────────────────────────────────────
-var versionCode = "ver. 05/10/2026 @ 07:56 PM"
+var versionCode = "ver. 05/11/26 @ 12:42 AM"
 var initialEncounterOverride = 0; // set to 5 to skip tutorial
 var TUTORIAL_SKIP_LOCALHOST = true;
 var RANKINGS_DISABLED_LOCALHOST = false;
--- version.sh
+++ version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Updates versionCode in js/constants.js to the current date/time
 
-TIMESTAMP=$(date "+%m/%d/%Y @ %I:%M %p")
+TIMESTAMP=$(date "+%m/%d/%y @ %I:%M %p")
 NEW_VERSION="ver. $TIMESTAMP"
 
 sed -i '' "s|var versionCode = \"ver\. [^\"]*\"|var versionCode = \"$NEW_VERSION\"|" js/constants.js
