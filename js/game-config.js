f361728d3679a7085629c286246f2e5187c0fbca
Adam Svoboda <eidamsvoboda@gmail.com>
2026-05-11 17:27:23 +0200

    update rarity tiers +2 smaller stats is rare

--- js/game-config.js
+++ js/game-config.js
@@ -95,8 +95,8 @@ var GAME_CONFIG = DIFFICULTY_MODES.Standard;
 var RARITY_TIERS = {
   Cursed:    { weight:  3, color: colorSoftRed,       bg: colorDarkRedSubtle, netMin: -Infinity, netMax: -0.01 },
   Common:    { weight: 60, color: colorWhite,         bg: '',                 netMin:  0,        netMax:  0.49 },
-  Uncommon:  { weight: 25, color: colorLightBlue,     bg: colorDarkBlue,      netMin:  0.5,      netMax:  1.49 },
-  Rare:      { weight: 10, color: colorPurple,        bg: colorDarkPurple,    netMin:  1.5,      netMax:  2.99 },
+  Uncommon:  { weight: 25, color: colorLightBlue,     bg: colorDarkBlue,      netMin:  0.5,      netMax:  0.99 },
+  Rare:      { weight: 10, color: colorPurple,        bg: colorDarkPurple,    netMin:  1,        netMax:  2.99 },
   Legendary: { weight:  2, color: colorOrange,        bg: colorDarkOrange,    netMin:  3.0,      netMax:  Infinity },
   Familiar:  { weight:  0, color: colorSoftGreen, bg: colorFamiliarGreen,     netMin:  0,        netMax:  0 }
 };
