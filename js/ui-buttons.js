edd0fb63e81eb84051ed83214d6f8454a9c9670e
Adam Svoboda <eidamsvoboda@gmail.com>
2026-05-18 17:10:01 +0200

    fix price vs button init for favor

--- js/ui-buttons.js
+++ js/ui-buttons.js
@@ -372,7 +372,7 @@ function adjustEncounterButtons(){
     case "Shop":
       var availableCoins=savedCoins-spentCoins;
       setButton('button_attack',"2 🪙 Favor",colorSoftGreen);
-        if (availableCoins<1) setButton('button_attack',"2 🪙 Favor",colorDarkGrey);
+        if (availableCoins<2) setButton('button_attack',"2 🪙 Favor",colorDarkGrey);
 
       setButton('button_roll',"👣 Leave",colorRed);
       if (availableCoins<=0) setButton('button_roll',"👣 Leave",colorYellow);
