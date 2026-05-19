b1e5f0b9802901d1610a1482c3b6da8039fbbcd6
Adam Svoboda <eidamsvoboda@gmail.com>
2026-05-19 02:13:29 +0200

    bump version

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 // ── Debug / Version ───────────────────────────────────────────────────────────
-var versionCode = "ver. 05/19/26 @ 02:12 AM"
+var versionCode = "ver. 05/19/26 @ 02:13 AM"
 var initialEncounterOverride = 0; // set to 5 to skip tutorial
 
 var SPLASH_DISABLED_LOCALHOST = true;
